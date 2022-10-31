4b462e2e1926e342bb57720d17fd5bca4a2f8663
bshop <jgbispo20@gmail.com>
2022-10-31 12:08:41 -0300

    utils for verify empty

---
+++ src/utils/VerifyEmpty.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Bank
+{
+  class VerifyEmpty
+  {
+    public static bool IsStringEmpty(string? value)
+    {
+      return string.IsNullOrEmpty(value);
+    }
+
+    public static bool IsIntEmpty(int value)
+    {
+      return value == 0;
+    }
+
+    public static bool IsDoubleEmpty(double value)
+    {
+      return value == 0;
+    }
+  }
+}
