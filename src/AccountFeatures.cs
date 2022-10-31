c84006662f8a70cbd88d6638c3d939b390d0dc09
bshop <jgbispo20@gmail.com>
2022-10-31 12:21:11 -0300

    Remove (using System)

---
+++ src/AccountFeatures.cs
@@ -0,0 +1,36 @@
+namespace Bank
+{
+  class AccountFeatures
+  {
+    public static void Transfer(Account originAccount, Account destinationAccount, double value)
+    {
+      if (originAccount.GetBalance() < value)
+      {
+        throw new Exception("Insufficient balance");
+      }
+
+      originAccount.Withdraw(value);
+      destinationAccount.Deposit(value);
+    }
+
+    public static void Withdraw(Account account, double value)
+    {
+      if (account.GetBalance() < value)
+      {
+        throw new Exception("Insufficient balance");
+      }
+
+      account.Withdraw(value);
+    }
+
+    public static void Deposit(Account account, double value)
+    {
+      account.Deposit(value);
+    }
+
+    public static void ShowBalance(Account account)
+    {
+      Console.WriteLine($"Current balance: {account.GetBalance()}");
+    }
+  }
+}
--- src/Person.cs
+++ src/Person.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Bank
 {
   class Person
